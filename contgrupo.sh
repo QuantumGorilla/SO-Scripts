8d8a16587411f1287a6d85a0bc9027200db3b8df
QuantumGorilla <manotasja@uninorte.edu.co>
2020-05-09 22:19:44 -0500

    contgrupo.sh ultimate fix!

--- contgrupo.sh
+++ contgrupo.sh
@@ -1,42 +1,28 @@
 #! /bin/bash
-echo Fecha actual:
-date
 #Nos posicionamos en la carpeta del usuario
 cd ~
-#Nombre de la carpeta que va a buscar
-echo Escriba el nombre del grupo
-read grupo
-#Comprueba que no este vacia la variable
-while [[ -z $grupo ]]
-do
-echo Por favor, escriba el nombre del grupo
-read grupo
-done
-#Ruta de la carpeta
-echo Escriba la ruta del directorio
-read ruta
-#Comprueba que la ruta no este vacia
-while [[ -z $ruta ]]
-do
-echo Por favor, escriba la ruta del directorio
-read ruta
-done
-#Busca la ruta
-if [ -d $ruta ]
-then
-cd $ruta
-#Busca el directorio
-if [ -d $grupo ]
-then
-cd $grupo
-#Cuenta los ficheros que se encuentren dentro del grupo especificado
-echo El número de ficheros en $grupo es:
-find . -type f | wc -l
+if [ -n "$1" ] && [ -n "$2" ]; then
+	grupo=$1
+	ruta=$2
+	#Busca la ruta
+	if [ -d $ruta ]; then
+		cd $ruta
+		#Busca el directorio
+		if [ -d $grupo ]; then
+			cd $grupo
+			#Cuenta los ficheros que se encuentren 			dentro del grupo especificado
+			echo Número de archivos del grupo $grupo en 				$ruta es:
+			find . -type f | wc -l
+			echo Ocultos:
+			ls -a | wc -l
+		else
+			#Si no existe el archivo imprime el error
+			echo "El grupo no existe"
+		fi
+	else
+	#Si no existe la ruta imprime el error
+	echo "La ruta no existe"
+	fi
 else
-#Si no existe el archivo imprime el error
-echo "El archivo no existe"
-fi
-else
-#Si no existe la ruta imprime el error
-echo "La ruta no existe"
+	echo No pasaron los correspondientes parametros: Grupo  		Ruta
 fi
