4158fe15f5ce0fed04890e2a5d66acf9d23f412a
QuantumGorilla <manotasja@uninorte.edu.co>
2020-05-10 00:19:15 -0500

    i swear this is the last commit :)

--- reciente.sh
+++ reciente.sh
@@ -6,8 +6,10 @@ aux='^-?[0-9]+([.][0-9]+)?$'
 if [ ! "$1" ]; then
 	echo Segundos desde la última modificación / Archivo modificado
 	find . -type f -printf "%T@ %p\n" | head -10
+	#Controlamos que el parámetro enviado es númerico
 	elif ! [[ $1 =~ $aux ]]; then
 		echo El parámetro enviado no es númerico
+	#Controlamos que sea un solo parámetro enviado
 	elif [ $# != 1 ]; then
 		echo Se envió más de un parámetro
 	else
