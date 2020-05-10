6c53158bb21ce5bf61557dd0b15e43ae398b6897
QuantumGorilla <manotasja@uninorte.edu.co>
2020-05-09 23:41:30 -0500

    contarcdir.sh finished

--- contarcdir.sh
+++ contarcdir.sh
@@ -1,14 +1,17 @@
 #! /bin/bash
 #Nos posicionamos en la carpeta del usuario
 cd ~
-echo Escriba el nombre del grupo
+#
+echo Nombre del directorio:
 read grupo
 ruta=$(find . -name $grupo)
 if [ "$ruta" ]; then
 	cd $ruta
 	#Cuenta archivos
 	archivos=$(find . -type f | wc -l)
-	echo Número de archivos: $archivos
+	echo Número de archivos es: $archivos
+	directorios=$(find . -type d | wc -l)
+	echo Número de directorios es: $directorios
 else
 	echo El directorio no existe
 fi
