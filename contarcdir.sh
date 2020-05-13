1106f23fb1518a4cbe98dd44ad7fc898f376ac85
QuantumGorilla <manotasja@uninorte.edu.co>
2020-05-13 11:05:02 -0500

    now counts the subdirectories

--- contarcdir.sh
+++ contarcdir.sh
@@ -10,8 +10,8 @@ if [ "$ruta" ]; then
 	#Cuenta archivos
 	archivos=$(find . -type f | wc -l)
 	echo Número de archivos es: $archivos
-	#Cuenta los directorios (incluido en el que se encuentra)
-	directorios=$(find . -type d | wc -l)
+	#Cuenta los subdirectorios (excluido en el que se encuentra)
+	directorios=$(find . -exec ls -l {} \; | grep '^d' | wc -l)
 	echo Número de directorios es: $directorios
 else
 	echo El directorio no existe
