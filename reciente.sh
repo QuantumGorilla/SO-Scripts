81d0e6de46eac7dbcf56c7779cff24fcf9369b30
QuantumGorilla <manotasja@uninorte.edu.co>
2020-05-18 00:01:57 -0500

    fixed bugs and minor errors

--- reciente.sh
+++ reciente.sh
@@ -1,11 +1,9 @@
 #! /bin/bash
-#Nos posicionamos en la carpeta del usuario
-cd ~
 #Expresión regular para controlar que sea númerico
 aux='^-?[0-9]+([.][0-9]+)?$'
 if [ ! "$1" ]; then
-	echo Segundos desde la última modificación / Archivo modificado
-	find . -type f -printf "%T@ %p\n" | head -10
+	echo Archivo modificado
+	find . -type f -printf "%p\n" | ls -t | head -10
 	#Controlamos que el parámetro enviado es númerico
 	elif ! [[ $1 =~ $aux ]]; then
 		echo El parámetro enviado no es númerico
@@ -13,6 +11,6 @@ if [ ! "$1" ]; then
 	elif [ $# != 1 ]; then
 		echo Se envió más de un parámetro
 	else
-	echo Segundos desde la última modificación / archivo modificado
-	find . -type f -printf "%T@ %p\n" | head -$1
+	echo Archivo modificado
+	find . -type f -printf "%p\n" | ls -t | head -$1
 fi
